0c6a4aff090e51e53e5cff8ef69646a7fa0d1fcb
Coding Rooms <git@codingrooms.io>
2025-10-04 00:20:35 +0000

    Challenge 1: Collatz sequence - while loop for unknown iterations

---
+++ akmutai1_assignment_5.py
@@ -0,0 +1,16 @@
+print("=== Challenge 1: Collatz Conjecture ===")
+current_number = int(input("Enter starting number: "))
+step_count = 0
+
+print(f"Sequence: {current_number}", end=" ")
+
+while current_number != 1:
+    if current_number % 2 == 0:
+        current_number //= 2
+    else:
+        current_number = 3 * current_number + 1
+    step_count += 1
+    print(current_number, end=" ")
+print()
+print(f"Steps: {step_count}")
+print()
